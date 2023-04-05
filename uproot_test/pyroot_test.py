cad416cafc7d369b9e939efc89e0bda12304c548
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-04-05 13:47:48 +0100

    tried to fix dedx vs rr plot

--- uproot_test/pyroot_test.py
+++ uproot_test/pyroot_test.py
@@ -3,36 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 root_file_dir= '/home/giorgio/Desktop/ROOT_files/'
-f= rt.TFile(root_file_dir + 'flat_caf_1-1637ea75-449f-467f-abd7-9e2f4f9710a7.root')
-tree= f.Get('recTree;31')
-leaves = tree.GetListOfLeaves()
-class PyListOfLeaves(dict) :
-    pass
+f= rt.TFile(root_file_dir + 'reco2-1cf8343b-b547-4ec5-9de8-da9ee53d258d.root')
+tree= f.Get('Events')
 
-pyl = PyListOfLeaves()
 
-for i in range(0,leaves.GetEntries() ) :
-    leaf = leaves.At(i)
-    name = leaf.GetName()
-    pyl.__setattr__(name,leaf)
-    if name == 'rec.reco.trk.calo.2.points.dedx':
-        break
 
-nev = tree.GetEntries()
-print(tree.GetLeaf('rec.reco.trk.calo.2.points.dedx').GetNdata())
+
+
+
 list_dedx= []
 list_rr= []
 list_end_x= []
 
 for entry in tree:
-    entry.Show()
-    #print(entry.GetLeaf('rec.reco.trk.calo.2.points.dedx').GetNdata())
-    print(entry.GetLeaf('rec.reco.trk.calo.2.points.dedx').GetLeafCounter())
-    #dedx= entry.GetLeaf('rec.reco.trk.calo.2.points.dedx')
-    #print(dedx)
-    ##rr= entry.GetLeaf('rec.reco.trk.calo.2.points.rr').GetValue()
-    #for i in dedx:
-        #print(dedx.GetValue())
+    rr= entry.GetLeaf('anab::Calorimetrys_pandoraCalo__Reco2./anab::Calorimetrys_pandoraCalo__Reco2.obj/anab::Calorimetrys_pandoraCalo__Reco2.obj.fdEdx')
+    print(rr)
         
 
     #if dedx<5 and rr<25:
